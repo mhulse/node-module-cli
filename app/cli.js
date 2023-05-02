1990a4839fe7a007fa7aed11cbcc1ed10c87c887
Michael Hulse <mickyhulse@gmail.com>
2023-05-02 11:19:41 -0700

    Adding stuff and things. :)

--- app/cli.js
+++ app/cli.js
@@ -1,40 +1,120 @@
 #!/usr/bin/env node
 
-import fs from "fs"
+import colors from "colors"
 import yargs from "yargs"
-import { hideBin } from "yargs/helpers" // This is shorthand for `process.argv.slice(2)`.
 
-const OPTIONS = {}
+import util from "./util.js"
 
-;(async function start() {
-  console.info("start")
+module.exports = (() => {
+  class GenericCLI {
+    constructor () {
+      this._argv = {}
+      this._allowed = {
+        letters: ["A", "B", "C", "D"],
+        numbers: [0, 1, 2],
+      }
+      this._options = {}
+    }
 
-  const parser = yargs(hideBin(process.argv))
-    .usage(`Usage: $0 -d [/path/to/directory/]`)
-    .option("directory", {
-      alias: ["d"],
-      description: "A directory!",
-      type: "string",
-      demand: true,
-    })
-    .alias("h", "help")
-    .help("h", "Show help.")
-    .fail(false)
+    async getOptions () {
+      const parser = yargs(hideBin(process.argv))
+        .usage(`Usage: $0 -d </path/to/directory/>`)
+        .option("directory", {
+          alias: [
+            "d",
+          ],
+          description: "Output directory?",
+          type: "string",
+          demand: true,
+        })
+        .option("letters", {
+          alias: [
+            "l",
+          ],
+          description: `Alphabet? Choose one: ${this._allowed.fisheye.join(", ")}`,
+          type: "string",
+        })
+        .option("numbers", {
+          alias: [
+            "n",
+          ],
+          description: `Numbers? Choose one: ${this._allowed.debug.join(", ")}`,
+          type: "number",
+        })
+        .alias("h", "help")
+        .help("h", "Show help.")
+        .fail(false)
+        .strict()
 
-  try {
-    const argv = await parser.parse();
-    console.info("Parsed!")
-  } catch (err) {
-    console.info(`${err.message}\n ${await parser.getHelp()}`)
+        try {
+          this._argv = await parser.parse();
+          console.info("Parsed!")
+        } catch (err) {
+          console.info(`${err.message}\n ${await parser.getHelp()}`)
+        }
+    }
+
+    async checkOptions () {
+      const results = {
+        directory: "set".green,
+        letters: "not set".yellow,
+        numbers: "not set".yellow,
+      }
+
+      // Yargs demanded this option:
+      this._options.directory = this._argv.directory;
+
+      if (
+        this._argv.letters
+        &&
+        (typeof this._argv.letters === 'string')
+        &&
+        this._allowed.letters.includes(this._argv.letters)
+      ) {
+        this._options.letters = this._argv.letters;
+        results.letters = this._argv.letters.green;
+      }
+
+      // ........ more to come ........
+    }
   }
-
-  console.info("finish")
-
-  try {
-    OPTIONS.directory = await fs.promises.realpath(parser.argv.directory);
-  } catch (err) {
-    console.error("Error occurred while reading directory!", err);
-  }
-
-  console.log(OPTIONS)
 })()
+
+// import fs from "fs"
+// import yargs from "yargs"
+// import { hideBin } from "yargs/helpers" // This is shorthand for `process.argv.slice(2)`.
+
+// const OPTIONS = {}
+
+// ;(async function start() {
+//   console.info("start")
+
+//   const parser = yargs(hideBin(process.argv))
+//     .usage(`Usage: $0 -d [/path/to/directory/]`)
+//     .option("directory", {
+//       alias: ["d"],
+//       description: "A directory!",
+//       type: "string",
+//       demand: true,
+//     })
+//     .alias("h", "help")
+//     .help("h", "Show help.")
+//     .fail(false)
+
+//   try {
+//     const argv = await parser.parse();
+//     console.info("Parsed!")
+//   } catch (err) {
+//     console.info(`${err.message}\n ${await parser.getHelp()}`)
+//   }
+
+//   console.info("finish")
+
+//   try {
+//     OPTIONS.directory = await fs.promises.realpath(parser.argv.directory);
+//   } catch (err) {
+//     console.error("Error occurred while reading directory!", err);
+//   }
+
+//   console.log(OPTIONS)
+// })()
--- app/util.js
+++ app/util.js
@@ -1,10 +1,21 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
+import path from "path"
+import { fileURLToPath } from "url"
+import fs from "fs"
+import untildify from "untildify"
 
 // TODO: make more modular, like: https://stackoverflow.com/a/30282425/922323
 export default {
   getDirName(moduleUrl) {
     const filename = fileURLToPath(moduleUrl)
     return path.dirname(filename)
-  }
+  },
+
+  async dirExists(dir) {
+    try {
+      await fs.promises.access(path.resolve(untildify(dir)))
+      return true
+    } catch (err) {
+      return false
+    }
+  },
 }
